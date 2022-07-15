f8c3113054e77560a964f66af5f81e19165ab89d
Samuco PC <boliviadd.@gmail.com>
2022-07-15 19:26:33 -0400

    cambios api

--- public/demo2/assets/js/app.js
+++ public/demo2/assets/js/app.js
@@ -1,88 +1,88 @@
-var items = [
+/* var items = [
     {
-        code: "C11253",
-        name: "Cliente 1",
-        address: "Calle Caballero y Beni",
-        distance: 0,
-        point: { lat: -17.780051, lng: -63.181219 }
+        id: "C11253",
+        rol: "Vendedor",
+        nombre: "vendedor 1",
+        color_map:"0A60EC",
+        pedidos: [
+            {
+                cliente:"Cliente 1",
+                direccion: "Calle Caballero y Beni",
+                lat: -17.780051,
+                lng: -63.181219 
+            },
+            {
+                cliente:"Cliente 2",
+                direccion: "Calle Mercado entre Isabel la Catolica y Av.Cañoto",
+                lat: -17.789572, 
+                lng: -63.188181 
+            },
+            {
+                cliente:"Cliente 3",
+                direccion: "Calle Rafael Peña y 24 de Septiembre",
+                lat: -17.777609, 
+                lng: -63.182088 
+            },
+            {
+                cliente:"Cliente 4",
+                direccion: "Calle Cuellar y Sara",
+                lat: -17.779734, 
+                lng: -63.187141 
+            },
+            {
+                cliente:"Cliente 5",
+                direccion: "Calle Corrdillera esquina Ingavi",
+                lat: -17.785026, 
+                lng: -63.186926 
+            },
+            {
+                cliente:"Cliente 6",
+                direccion: "Calle Atenas entre San Antonio y 16 de Julio",
+                lat: -17.826734, 
+                lng: -63.199669 
+            },
+            
+        ],
+        totalPedidos:10
     },
     {
-        code: "C11262",
-        name: "Cliente 2",
-        address: "Calle Mercado entre Isabel la Catolica y Av.Cañoto",
-        distance: 0,
-        point: { lat: -17.789572, lng: -63.188181 }
+        id: "C66215",
+        rol: "Vendedor",
+        nombre: "vendedor 2",
+        color_map:"E20AEC",
+        pedidos: [
+            {
+                cliente:"Cliente 1",
+                direccion: "Calle España y Ayacucho",
+                lat: -17.783851, lng: -63.184909
+            },
+            {
+                cliente:"Cliente 2",
+                direccion: "Av.Santa Cruz esquina Cap.Mariano Arrien",
+                lat: -17.794935, lng: -63.182538
+            },
+            {
+                cliente:"Cliente 3",
+                direccion: "4to Anillo esquina Ave de Playa, Cambodromo",
+                lat: -17.755219, lng: -63.155707
+            },
+            {
+                cliente:"Cliente 4",
+                direccion: "Av.Virgen de Cotoca, Pampa de la Isla",
+                lat: -17.770039, lng: -63.124803
+            },
+            {
+                cliente:"Cliente 5",
+                direccion: "Colinas del Urubo, Urubo",
+                lat: -17.752018, lng: -63.218285
+            },
+        ],
+        totalPedidos:10
     },
-    {
-        code: "C11255",
-        name: "Cliente 3",
-        address: "Calle Rafael Peña y 24 de Septiembre",
-        distance: 0,
-        point: { lat: -17.777609, lng: -63.182088 }
-    },
-    {
-        code: "C11254",
-        name: "Cliente 4",
-        address: "Calle Cuellar y Sara",
-        distance: 0,
-        point: { lat: -17.779734, lng: -63.187141 }
-    },
-    {
-        code: "C11260",
-        name: "Cliente 5",
-        address: "Calle Corrdillera esquina Ingavi",
-        distance: 0,
-        point: { lat: -17.785026, lng: -63.186926 }
-    },
-    {
-        code: "C11280",
-        name: "Cliente 6",
-        address: "Calle Atenas entre San Antonio y 16 de Julio",
-        distance: 0,
-        point: { lat: -17.826734, lng: -63.199669 }
-    },
-    {
-        code: "C11261",
-        name: "Cliente 7",
-        address: "Calle España y Ayacucho",
-        distance: 0,
-        point: { lat: -17.783851, lng: -63.184909 }
-    },
-    {
-        code: "C11264",
-        name: "Cliente 8",
-        distance: 0,
-        point: { lat: -17.793337, lng: -63.166361 }
-    },
-    {
-        code: "C11263",
-        name: "Cliente 9",
-        address: "Av.Santa Cruz esquina Cap.Mariano Arrien",
-        distance: 0,
-        point: { lat: -17.794935, lng: -63.182538 }
-    },
-    {
-        code: "C11275",
-        name: "Cliente 10",
-        address: "4to Anillo esquina Ave de Playa, Cambodromo",
-        distance: 0,
-        point: { lat: -17.755219, lng: -63.155707 }
-    },
-    {
-        code: "C11276",
-        name: "Cliente 11",
-        address: "Av.Virgen de Cotoca, Pampa de la Isla",
-        distance: 0,
-        point: { lat: -17.770039, lng: -63.124803 }
-    },
-    {
-        code: "C11277",
-        name: "Cliente 12",
-        address: "Colinas del Urubo, Urubo",
-        distance: 0,
-        point: { lat: -17.752018, lng: -63.218285 }
-    }
 ];
+ */
+
+var items = null;
 
 var markers = Array();
 
@@ -94,26 +94,46 @@ var center = { lat: -17.783443, lng: -63.183526 };
 
 var map = null;
 
+var poligono = Array();
 var firstCircle = null;
+var response2 = null;
+var url = 'https://terbol.info/ServicioMod04/api/actividad?parameters.id_usuario=1&parameters.fecha_ini=01%2F01%2F2022&parameters.fecha_fin=15%2F07%2F2022';
 
 $(document).ready(function () {
+/*     console.log(items.data.oActividad.length);
+
+    for (let index = 0; index < items.length; index++) {
+        
+        console.log(items[index].oUsuario[0].nombre);
+        
+    } */
+
     console.log("Start App");
     loadMap();
-    loadItems();
-    drawMarkers();
+    getDataAsync();
 
-    $("#calc").click(function (event) {
-        orderMarkers();
-        loadItems();
-        drawMarkers();
-        event.preventDefault();
 
+    $("ol li").click(function(){
+        console.log($(this).attr("id"));
+        let dato = items.filter(item => item.id == $(this).attr("id").toString());
+        drawMarkersVendedor(dato);
+    
+        $("#calc").click(function (event) {
+            orderMarkers();
+            loadItems();
+            drawMarkersVendedor(items);
+            event.preventDefault();
+    
+        });
+    
+        $("#print").click(function (event) {
+            printMap();
+            event.preventDefault();
+        });
     });
 
-    $("#print").click(function (event) {
-        printMap();
-        event.preventDefault();
-    });
+
+
 });
 
 function renumerateItems() {
@@ -122,11 +142,11 @@ function renumerateItems() {
     });
 }
 
-function loadItems() {
+function loadItems(fetchedData) {
 
     $(".items .list").html("");
 
-    for (i = 0; i < items.length; i++) {
+/*     for (i = 0; i < items.length; i++) {
         var li = $("<li>", {
             lat: items[i].point.lat,
             lng: items[i].point.lng,
@@ -139,6 +159,26 @@ function loadItems() {
         index.html((i + 1).toString());
         li.append(index);
         div.html("[" + items[i].code + "] " + items[i].name);
+        li.append(div);
+        $(".items .list").append(li);dmso
+    } */
+
+        items = fetchedData.data.oActividad;
+
+        for (let i = 0; i < items.length; i++) {
+            var li = $("<li>",{
+                id: items[i].oUsuario.id_usuario,
+                rol: "usuario",
+                nombre: items[i].oUsuario.nombre,
+                color_map: items[i].oUsuario.color_map,
+                totalPedidos: items.length
+
+            });
+        var index = $("<div>", { class: "index" });
+        var div = $("<div>", { class: "text" });
+        index.html((i + 1).toString());
+        li.append(index);
+        div.html("[" + items[i].oCliente.id_cliente + "] " + items[i].oCliente.nombre + "(" + items[i].totalPedidos +")");
         li.append(div);
         $(".items .list").append(li);
     }
@@ -200,19 +240,108 @@ function drawMarkers() {
     path.setMap(map);
 }
 
-function clearMarkers() {
+function drawMarkersVendedor(params){
+    clearMarkers();
+    counter = 1;
+    color_map = "";
+    if (path != null) {
+        path.setMap(null);
+    }
+    for (let i = 0; i < params.length; i++) {
+        color_map = params[i].color_map;
+        for (let j = 0; j < params[i].pedidos.length; j++) {
 
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: parseFloat(params[i].pedidos[j].lat),
+                    lng: parseFloat(params[i].pedidos[j].lng)
+                },
+                label: ((counter)).toString(),
+                title: "[" + params[i].pedidos[j].cliente+ "] " + params[i].pedidos[j].direccion,
+                map: map
+            });
+            counter += 1;
+            markers.push(marker);
+            paths.push({ lat: parseFloat(params[i].pedidos[j].lat), lng: parseFloat(params[i].pedidos[j].lng) });
+        }
     }
 
-    markers = Array();
+    path = new google.maps.Polyline({
+        path: paths,
+        geodesic: true,
+        strokeColor: "#" + color_map,
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+    });
 
-    paths = Array();
+    path.setMap(map);
+}
+
+function drawMarkersAllVendedor(params){
+    console.log(params);
+    clearMarkers();
+    counter = 1;
+    color_map = "";
+
+    for (let i = 0; i < params.length; i++) {
+        color_map = params[i].oUsuario.color_map;
+        var pinImage = {
+            url: "http://www.googlemapsmarkers.com/v1/"+ params[i].oUsuario.color_map,
+          };
+
+
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: parseFloat(params[i].oCliente.latitud),
+                    lng: parseFloat(params[i].oCliente.longitud)
+                },
+                label: ((counter)).toString(),
+                //animation: google.maps.Animation.DROP,
+                title: "[" + params[i].pedidos[j].cliente+ "] " + params[i].pedidos[j].direccion,
+                icon: pinImage,
+                map: map
+            });
+            counter += 1;
+            markers.push(marker);
+            paths.push({ lat: parseFloat(params[i].pedidos[j].lat), lng: parseFloat(params[i].pedidos[j].lng) });
+
+        counter = 1;
+        path = new google.maps.Polyline({
+            path: paths,
+            geodesic: true,
+            strokeColor: "#" + color_map,
+            strokeOpacity: 1.0,
+            strokeWeight: 2,
+        });
+        path.setMap(map);
+
+        paths = [];
+
+    }
+
+
+
+}
+
+
+function clearMarkers() {
+
+
 
     if (path != null) {
         path.setMap(null);
     }
+
+
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers.length = 0;
+    markers = Array();
+
+    paths = Array();
+
+    
 }
 
 function loadMap() {
@@ -221,7 +350,7 @@ function loadMap() {
         center: center
     });
 
-    new google.maps.Circle({
+/*     new google.maps.Circle({
         strokeColor: "#FF0000",
         strokeOpacity: 0.8,
         strokeWeight: 2,
@@ -230,7 +359,7 @@ function loadMap() {
         map: map,
         center: center,
         radius: 100
-    });
+    }); */
 }
 
 function printMap() {
@@ -244,8 +373,8 @@ function orderMarkers() {
 
     $("ol li").each(function (index) {
         if (index == 0) {
-            first = Enumerable.From(items).Where("$.code=='" + $(this).attr("code") + "'").Select("$").FirstOrDefault();
-            others = Enumerable.From(items).Where("$.code!='" + $(this).attr("code") + "'").Select("$").ToArray();
+            first = Enumerable.From(items).Where("$.id=='" + $(this).attr("id") + "'").Select("$").FirstOrDefault();
+            others = Enumerable.From(items).Where("$.id!='" + $(this).attr("id") + "'").Select("$").ToArray();
         }
     });
 
@@ -260,7 +389,7 @@ function getRoute(first, others, result) {
     if (others.length > 0) {
         next = getNearest(first, others);
         result.push(next);
-        others = Enumerable.From(others).Where("$.code!='" + next.code + "'").Select("$").ToArray();
+        others = Enumerable.From(others).Where("$.id!='" + next.code + "'").Select("$").ToArray();
         getRoute(next, others, result);
     }
 }
@@ -278,4 +407,25 @@ function distanceBetween(point1, point2) {
     var b = point1.lng - point2.lng;
 
     return Math.sqrt(a * a + b * b);
+}
+async function getDataAsync() { 
+    let fetchedData; 
+    const response = await fetch(url) 
+    .then(response => response.json()) 
+    .then(json => fetchedData = json); 
+    console.log(fetchedData.data.oActividad);
+
+    loadItems(fetchedData);
+
+    $("#todos").click(function(event){
+        drawMarkersAllVendedor(fetchedData.data.oActividad);
+        event.preventDefault();
+    });
+
+
+/* 
+    for (let index = 0; index < fetchedData.data.oActividad.length; index++) {
+        items.push(fetchedData.data.oActividad[index]);
+                
+    } */
 }
