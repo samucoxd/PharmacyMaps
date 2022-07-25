ea0201f63943b999e1fb49cf8c5a1a3dc05a6f5b
Samuco PC <boliviadd.@gmail.com>
2022-07-25 17:40:20 -0400

    colores de item por vendedor

---
+++ public/demo2/assets/js/modeladoJson.js
@@ -0,0 +1,33 @@
+{
+    "status": "string",
+    "message": "string",
+    "data": {
+      "oActividad": [
+        {
+          "id_actividad": 0,
+          "pedido": true,
+          "cobranza": true,
+          "visita": true,
+          "cerrado": true,
+          "glosa": "string",
+          "fecha": "string",
+          "oCliente": {
+            "id_cliente": 0,
+            "nombre": "string",
+            "direccion": "string",
+            "telefono": "string",
+            "latitud": "string",
+            "longitud": "string",
+            "hora_inicial": "string",
+            "hora_final": "string"
+          },
+          "oUsuario": {
+            "id_usuario": 0,
+            "nombre": "string",
+            "color_map": "string",
+            "usuario": "string"
+          }
+        }
+      ]
+    }
+  }
